edc9e686f090b671945015a5bd9477ddc8379e19
AliceeeSixes <6d6perturn@gmail.com>
2025-02-12 11:20:37 +0000

    Basic layout for "our products" section

---
+++ js/application.js
@@ -0,0 +1,14 @@
+
+
+// Slick carousels
+
+$("#products-slider").slick({
+    // Arrows
+    arrows: true,
+    prevArrow: "#products-arrow-prev",
+    nextArrow: "#products-arrow-next",
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    slidesToShow: 3
+});
